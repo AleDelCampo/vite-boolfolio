980e722d2702ef8a4d43b3c168d118c106b2cf86
andrealivornese <andrealivornese@gmail.com>
2024-04-29 18:12:11 +0200

    feat: axios call

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script>
 
+import axios from 'axios';
+
+export default{
+  data(){
+    return{
+      url : 'http://127.0.0.1:8000',
+    }
+  },
+  mounted(){
+
+    axios.get(this.url).then(res => {
+
+      console.log(res);
+
+    });
+
+  },
+  methods:{
+
+  }
+}
+
 </script>
 
 <template>
