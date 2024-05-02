2cebadfc3d04851e5aab7657fd7158ad33a3f6c7
AleHashirama <alehashirama@gmail.com>
2024-05-02 12:48:04 +0200

    Add: Next & Previous buttons work

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,47 +1,49 @@
 <script>
-
 import axios from 'axios';
+
 export default {
-    name: 'HomePage',
-    data() {
-        return {
-        projects: [],
-
-        apiLinks: [],
-        apiPageNumber: 1,
-        isLoading: true,
-        baseApiUrl: 'http://127.0.0.1:8000/api',
+  name: 'HomePage',
+  data() {
+    return {
+      projects: [],
+      apiLinks: [],
+      apiPageNumber: 1,
+      isLoading: true,
+      baseApiUrl: 'http://127.0.0.1:8000/api',
+      currentPage: 1,
+    }
+  },
+  mounted() {
+    this.apiCall();
+  },
+  methods: {
+    apiCall() {
+      this.isLoading = true;
+      axios.get(this.baseApiUrl + '/projects', {
+        params: {
+          page: this.apiPageNumber
         }
+      }).then(res => {
+        if (res.data.success) {
+          this.isLoading = false;
+        }
+        this.projects = res.data.results.data;
+        this.apiLinks = res.data.results.links;
+      })
     },
-    mounted() {
-        
-        this.apiCall();
-    },
-    methods: {
-
-        apiCall() {
-
-        this.isLoading = true
-        axios.get(this.baseApiUrl + '/projects', {
-            params: {
-            page: this.apiPageNumber
-            }
-        }).then(res => {
-
-            if(res.data.success) {
-
-            this.isLoading = false
-            }
-            this.projects = res.data.results.data;
-            this.apiLinks = res.data.results.links;
+    changeApiPage(pageUrl) {
+      this.isLoading = true;
+      axios.get(pageUrl)
+        .then(response => {
+          this.projects = response.data.results.data;
+          this.apiLinks = response.data.results.links;
+          this.isLoading = false;
         })
-        },
-        changeApiPage(pageNumber) {
-            
-        this.apiPageNumber = pageNumber;
-        this.apiCall();
-        },
     },
+    setCurrentPage(pageNumber) {
+      this.currentPage = pageNumber;
+    }
+  },
 }
 </script>
 
@@ -53,19 +55,26 @@ export default {
     <div v-if="!isLoading">
 
       <ul>
-        <li v-for="project in projects" class="mb-2">
-            {{ project.title }} <router-link :to="{name: 'single-project', params: {slug: project.slug}}" class="btn btn-outline-info btn-outline">Mostra</router-link>
+        <li v-for="project in projects" :key="project.slug" class="mb-2">
+          {{ project.title }} <router-link :to="{ name: 'single-project', params: { slug: project.slug } }"
+            class="btn btn-outline-info btn-outline">Mostra</router-link>
         </li>
       </ul>
 
-      <nav >
+      <nav>
         <ul class="d-flex gap-2">
-
-          <li v-for="link in apiLinks" v-html="link.label" @click="changeApiPage(link.label)" :class="link.label == apiPageNumber ? 'active' : ''">
-
+          <li v-if="apiLinks.prev_page_url">
+            <button @click="changeApiPage(apiLinks.prev_page_url); setCurrentPage(currentPage - 1)">Previous</button>
           </li>
 
-        </ul>      
+          <li v-for="link in apiLinks" :key="link.url" v-html="link.label"
+            @click="changeApiPage(link.url); setCurrentPage(link.label)" :class="{ active: link.label == currentPage }">
+          </li>
+
+          <li v-if="apiLinks.next_page_url">
+            <button @click="changeApiPage(apiLinks.next_page_url); setCurrentPage(currentPage + 1)">Next</button>
+          </li>
+        </ul>
       </nav>
     </div>
     <div v-else>
@@ -82,18 +91,21 @@ nav {
   margin-top: 100px;
   padding-top: 20px;
   border-top: solid 1px gray;
+
   ul {
     list-style-type: none;
-    
+
     li {
-      
+
       padding: 8px;
       text-decoration: none;
       color: white;
       transition: all .3s ease;
       cursor: pointer;
-      &:hover, &.active {
-        background-color: rgba(255,255,255,0.4);
+
+      &:hover,
+      &.active {
+        background-color: rgba(255, 255, 255, 0.4);
         color: black;
       }
     }
